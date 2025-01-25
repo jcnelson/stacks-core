48415b675da86b76e03879b1faf5d25a6d7d53d5
Jude Nelson <jude@stacks.org>
2025-01-24 21:30:51 -0500

    chore: make playground better with an option to terminate, and choose the right keys

--- testnet/stacks-node/src/tests/wrb_integrations.rs
+++ testnet/stacks-node/src/tests/wrb_integrations.rs
@@ -17,39 +17,31 @@
 
 use std::collections::HashMap;
 use std::sync::atomic::{AtomicU64, Ordering};
-use std::sync::{Arc};
+use std::sync::Arc;
 use std::time::{Duration, Instant};
 use std::{env, thread};
 
+use clarity::vm::costs::ExecutionCost;
 use clarity::vm::types::{PrincipalData, QualifiedContractIdentifier};
 use clarity::vm::Value;
-
 use stacks::chainstate::nakamoto::test_signers::TestSigners;
-use stacks::chainstate::nakamoto::{NakamotoChainState};
+use stacks::chainstate::nakamoto::NakamotoChainState;
 use stacks::chainstate::stacks::db::StacksChainState;
-use stacks_common::util::secp256k1::Secp256k1PrivateKey;
 use stacks_common::util::hash::Hash160;
+use stacks_common::util::secp256k1::Secp256k1PrivateKey;
 
 use super::bitcoin_regtest::BitcoinCoreController;
-use crate::neon::{Counters};
+use crate::neon::Counters;
 use crate::run_loop::boot_nakamoto;
+use crate::tests::nakamoto_integrations::{
+    blind_signer, boot_to_epoch_3, naka_neon_integration_conf, next_block_and_mine_commit,
+    setup_stacker, wait_for,
+};
 use crate::tests::neon_integrations::{
-    get_chain_info_result,
-    submit_tx,
-    test_observer,
-    wait_for_runloop,
+    call_read_only, get_chain_info_result, submit_tx, test_observer, wait_for_runloop,
 };
-use crate::tests::{
-    make_contract_publish,
-    make_contract_call,
-};
+use crate::tests::{make_contract_call, make_contract_publish};
 use crate::{tests, BitcoinRegtestController, BurnchainController};
-use crate::tests::nakamoto_integrations::naka_neon_integration_conf;
-use crate::tests::nakamoto_integrations::setup_stacker;
-use crate::tests::nakamoto_integrations::boot_to_epoch_3;
-use crate::tests::nakamoto_integrations::blind_signer;
-use crate::tests::nakamoto_integrations::next_block_and;
-use crate::tests::nakamoto_integrations::wait_for;
 
 pub const BNS_V2: &str = include_str!("./bnsv2/BNS-V2.clar");
 pub const COMMISSION_TRAIT: &str = include_str!("./bnsv2/commission-trait.clar");
@@ -60,9 +52,9 @@ pub const ZONEFILE_RESOLVER: &str = include_str!("./bnsv2/zonefile-resolver.clar
 pub const HOME_WRBPOD: &str = r#"
 (define-constant OWNER tx-sender)
 (define-constant NUM_SLOTS u64)
-(define-constant CHUNK_SIZE u65536)
+(define-constant CHUNK_SIZE (* u1024 u1024))
 (define-constant WRITE_FREQ u1)
-(define-constant MAX_WRITES u340282366920938463463374607431768211455)
+(define-constant MAX_WRITES u4294967295)
 (define-constant MAX_NEIGHBORS u8)
 (define-constant HINT_REPLICAS (list ))
 
@@ -84,7 +76,7 @@ pub const APP_WRBPOD: &str = r#"
 (define-constant NUM_SLOTS u1)
 (define-constant CHUNK_SIZE (* u1024 u1024))
 (define-constant WRITE_FREQ u1024)
-(define-constant MAX_WRITES u340282366920938463463374607431768211455)
+(define-constant MAX_WRITES u4294967295)
 (define-constant MAX_NEIGHBORS u8)
 (define-constant HINT_REPLICAS (list ))
 
@@ -122,6 +114,8 @@ pub fn wrb_playground() {
         return;
     }
 
+    let terminate = env::var("TERMINATE") == Ok("1".into());
+
     let http_port_str = env::var("WRB_PORT")
         .map(|s| s.to_string())
         .unwrap_or("30443".to_string());
@@ -137,36 +131,45 @@ pub fn wrb_playground() {
     let http_origin = format!("http://{}", &naka_conf.node.rpc_bind);
     naka_conf.miner.wait_on_interim_blocks = Duration::from_secs(1);
 
-    let sender_sk = Secp256k1PrivateKey::from_hex("e89bb394ecd5161007a84b34ac98d4f7239016c91d3e0c7c3b97aa4996932883").unwrap();
-    let home_sk = Secp256k1PrivateKey::from_hex("05dbe6a6760e62ebda8d6817f857751f9daf7ab17d91d775613eff7d09a4eaaf").unwrap();
-    let app_sk = Secp256k1PrivateKey::from_hex("1c9e5672eb18d703b984f9fb9ff74a8e2d889fb0165014ab8ec74701701ba20a").unwrap();
+    let sender_sk = Secp256k1PrivateKey::from_hex(
+        "e89bb394ecd5161007a84b34ac98d4f7239016c91d3e0c7c3b97aa499693288301",
+    )
+    .unwrap();
+    let home_sk = Secp256k1PrivateKey::from_hex(
+        "05dbe6a6760e62ebda8d6817f857751f9daf7ab17d91d775613eff7d09a4eaaf01",
+    )
+    .unwrap();
+    let app_sk = Secp256k1PrivateKey::from_hex(
+        "1c9e5672eb18d703b984f9fb9ff74a8e2d889fb0165014ab8ec74701701ba20a01",
+    )
+    .unwrap();
 
     let sender_signer_sk = Secp256k1PrivateKey::new();
     let sender_signer_addr = tests::to_addr(&sender_signer_sk);
     let mut signers = TestSigners::new(vec![sender_signer_sk]);
-    
+
     let sender_addr = tests::to_addr(&sender_sk);
     let home_addr = tests::to_addr(&home_sk);
     let app_addr = tests::to_addr(&app_sk);
-    naka_conf.add_initial_balance(
-        PrincipalData::from(sender_addr).to_string(),
-        10000000
-    );
-    naka_conf.add_initial_balance(
-        PrincipalData::from(home_addr).to_string(),
-        10000000
-    );
-    naka_conf.add_initial_balance(
-        PrincipalData::from(app_addr).to_string(),
-        1000000000000
-    );
+    naka_conf.add_initial_balance(PrincipalData::from(sender_addr).to_string(), 10000000);
+    naka_conf.add_initial_balance(PrincipalData::from(home_addr).to_string(), 10000000);
+    naka_conf.add_initial_balance(PrincipalData::from(app_addr).to_string(), 1000000000000);
     naka_conf.add_initial_balance(PrincipalData::from(sender_signer_addr).to_string(), 100000);
-    
+
     // subscribe to wrbpods
-    naka_conf.node.stacker_dbs = vec![
+    naka_conf.node.stacker_dbs.append(&mut vec![
         QualifiedContractIdentifier::parse(&format!("{}.home-wrbpod", &home_addr)).unwrap(),
-        QualifiedContractIdentifier::parse(&format!("{}.app-wrbpod", &app_addr)).unwrap()
-    ];
+        QualifiedContractIdentifier::parse(&format!("{}.app-wrbpod", &app_addr)).unwrap(),
+    ]);
+
+    // expand call-readonly
+    naka_conf.connection_options.read_only_call_limit = ExecutionCost {
+        write_length: 150000000,
+        write_count: 50000,
+        read_length: 1000000000,
+        read_count: 50000,
+        runtime: 100000000000,
+    };
 
     let stacker_sk = setup_stacker(&mut naka_conf);
 
@@ -186,7 +189,6 @@ pub fn wrb_playground() {
         blocks_processed,
         naka_submitted_commits: commits_submitted,
         naka_proposed_blocks: proposals_submitted,
-        naka_mined_blocks: mined_blocks,
         ..
     } = run_loop.counters();
 
@@ -229,17 +231,27 @@ pub fn wrb_playground() {
     blind_signer(&naka_conf, &signers, proposals_submitted);
 
     wait_for_first_naka_block_commit(60, &commits_submitted);
-     
+
+    next_block_and_mine_commit(
+        &mut btc_regtest_controller,
+        60,
+        &coord_channel,
+        &commits_submitted,
+    )
+    .unwrap();
+
     let mut nonces = HashMap::new();
 
     // instantiate BNSv2
-    for (i, (name, key, code)) in [
+    for (name, key, code) in [
         ("commission-trait", &sender_sk, COMMISSION_TRAIT),
         ("sip-09", &sender_sk, SIP_009_TRAIT),
         ("BNS-V2", &sender_sk, BNS_V2),
         ("zonefile-resolver", &sender_sk, ZONEFILE_RESOLVER),
         ("namespace-airdrop", &sender_sk, NAMESPACE_AIRDROP),
-    ].iter().enumerate() {
+    ]
+    .iter()
+    {
         let addr = tests::to_addr(key);
         let nonce = *nonces.get(&addr).unwrap_or(&0);
         let contract_tx = make_contract_publish(
@@ -248,54 +260,19 @@ pub fn wrb_playground() {
             (code.len() * 10) as u64,
             naka_conf.burnchain.chain_id,
             name,
-            code
+            code,
         );
         nonces.insert(addr, nonce + 1);
 
         let info_before = get_chain_info_result(&naka_conf).unwrap();
         submit_tx(&http_origin, &contract_tx);
-  
-        if i == 0 {
-            let blocks_before = mined_blocks.load(Ordering::SeqCst);
-            let blocks_processed_before = coord_channel
-                .lock()
-                .expect("Mutex poisoned")
-                .get_stacks_blocks_processed();
-            next_block_and(&mut btc_regtest_controller, 60, || {
-                let blocks_count = mined_blocks.load(Ordering::SeqCst);
-                let blocks_processed = coord_channel
-                    .lock()
-                    .expect("Mutex poisoned")
-                    .get_stacks_blocks_processed();
-                Ok(blocks_count > blocks_before && blocks_processed > blocks_processed_before)
-            })
-            .unwrap();
 
-            let blocks_processed_before = coord_channel
-                .lock()
-                .expect("Mutex poisoned")
-                .get_stacks_blocks_processed();
-            let mined_before = test_observer::get_mined_nakamoto_blocks();
-            let commits_before = commits_submitted.load(Ordering::SeqCst);
-            info!("----- Waiting for deploy txs to be mined -----");
-            wait_for(30, || {
-                let blocks_processed = coord_channel
-                    .lock()
-                    .expect("Mutex poisoned")
-                    .get_stacks_blocks_processed();
-                Ok(blocks_processed > blocks_processed_before
-                    && test_observer::get_mined_nakamoto_blocks().len() > mined_before.len()
-                    && commits_submitted.load(Ordering::SeqCst) > commits_before)
-            })
-            .expect("Timed out waiting for interim blocks to be mined");
-        }
-        else {
-            // wait for it to be mined
-            wait_for(60, || {
-                let info = get_chain_info_result(&naka_conf).unwrap();
-                Ok(info.stacks_tip_height > info_before.stacks_tip_height)
-            }).unwrap();
-        }
+        // wait for it to be mined
+        wait_for(60, || {
+            let info = get_chain_info_result(&naka_conf).unwrap();
+            Ok(info.stacks_tip_height > info_before.stacks_tip_height)
+        })
+        .unwrap();
     }
 
     // activate BNS-V2
@@ -308,23 +285,26 @@ pub fn wrb_playground() {
         &sender_addr,
         "BNS-V2",
         "flip-migration-complete",
-        &[]
+        &[],
     );
     nonces.insert(sender_addr.clone(), nonce + 1);
-    
+
     // wait for it to be mined
     let info_before = get_chain_info_result(&naka_conf).unwrap();
     submit_tx(&http_origin, &bns_activate_tx);
     wait_for(60, || {
         let info = get_chain_info_result(&naka_conf).unwrap();
         Ok(info.stacks_tip_height > info_before.stacks_tip_height)
-    }).unwrap();
+    })
+    .unwrap();
 
     // set up wrbpods
     for (name, key, code) in [
         ("home-wrbpod", &home_sk, HOME_WRBPOD),
-        ("app-wrbpod", &app_sk, APP_WRBPOD)
-    ].iter() {
+        ("app-wrbpod", &app_sk, APP_WRBPOD),
+    ]
+    .iter()
+    {
         let addr = tests::to_addr(key);
         let nonce = *nonces.get(&addr).unwrap_or(&0);
         let contract_tx = make_contract_publish(
@@ -333,7 +313,7 @@ pub fn wrb_playground() {
             (code.len() * 10) as u64,
             naka_conf.burnchain.chain_id,
             name,
-            code
+            code,
         );
         nonces.insert(addr, nonce + 1);
 
@@ -344,13 +324,34 @@ pub fn wrb_playground() {
         wait_for(60, || {
             let info = get_chain_info_result(&naka_conf).unwrap();
             Ok(info.stacks_tip_height > info_before.stacks_tip_height)
-        }).unwrap();
+        })
+        .unwrap();
     }
 
     // register a name and load up an app
     let name = "wrb-playground";
     let namespace = "id";
-    
+
+    // get the name price
+    let price_value = call_read_only(
+        &naka_conf,
+        &sender_addr,
+        "BNS-V2",
+        "get-name-price",
+        vec![
+            &Value::buff_from(namespace.as_bytes().to_vec()).unwrap(),
+            &Value::buff_from(name.as_bytes().to_vec()).unwrap(),
+        ],
+    );
+    eprintln!("Price of {}.{} is {:?}", name, namespace, &price_value);
+    let price = price_value
+        .expect_result_ok()
+        .unwrap()
+        .expect_result_ok()
+        .unwrap()
+        .expect_u128()
+        .unwrap();
+
     let nonce = *nonces.get(&app_addr).unwrap_or(&0);
     let preorder_tx = make_contract_call(
         &app_sk,
@@ -361,37 +362,40 @@ pub fn wrb_playground() {
         "BNS-V2",
         "name-preorder",
         &vec![
-            Value::buff_from(Hash160::from_data(format!("{}.{}.salt", name, namespace).as_bytes()).0.to_vec()).unwrap(),
-            Value::UInt(6_400_000_000)
-        ]
+            Value::buff_from(
+                Hash160::from_data(format!("{}.{}.salt", name, namespace).as_bytes())
+                    .0
+                    .to_vec(),
+            )
+            .unwrap(),
+            Value::UInt(price),
+        ],
     );
     nonces.insert(app_addr.clone(), nonce + 1);
-    
+
     // wait for it to be mined
     let info_before = get_chain_info_result(&naka_conf).unwrap();
     submit_tx(&http_origin, &preorder_tx);
     wait_for(60, || {
         let info = get_chain_info_result(&naka_conf).unwrap();
         Ok(info.stacks_tip_height > info_before.stacks_tip_height)
-    }).unwrap();
-
-    // Wait for the tenure change payload to be mined
-    let blocks_before = mined_blocks.load(Ordering::SeqCst);
-    let blocks_processed_before = coord_channel
-        .lock()
-        .expect("Mutex poisoned")
-        .get_stacks_blocks_processed();
-    let commits_before = commits_submitted.load(Ordering::SeqCst);
-    next_block_and(&mut btc_regtest_controller, 60, || {
-        let blocks_count = mined_blocks.load(Ordering::SeqCst);
-        let blocks_processed = coord_channel
-            .lock()
-            .expect("Mutex poisoned")
-            .get_stacks_blocks_processed();
-        Ok(blocks_count > blocks_before
-            && blocks_processed > blocks_processed_before
-            && commits_submitted.load(Ordering::SeqCst) > commits_before)
     })
+    .unwrap();
+
+    next_block_and_mine_commit(
+        &mut btc_regtest_controller,
+        60,
+        &coord_channel,
+        &commits_submitted,
+    )
+    .unwrap();
+
+    next_block_and_mine_commit(
+        &mut btc_regtest_controller,
+        60,
+        &coord_channel,
+        &commits_submitted,
+    )
     .unwrap();
 
     // register it
@@ -407,8 +411,8 @@ pub fn wrb_playground() {
         &vec![
             Value::buff_from(namespace.as_bytes().to_vec()).unwrap(),
             Value::buff_from(name.as_bytes().to_vec()).unwrap(),
-            Value::buff_from(".salt".as_bytes().to_vec()).unwrap()
-        ]
+            Value::buff_from(".salt".as_bytes().to_vec()).unwrap(),
+        ],
     );
     nonces.insert(app_addr.clone(), nonce + 1);
 
@@ -421,10 +425,9 @@ pub fn wrb_playground() {
     })
     .unwrap();
 
-    // publish the app to the app's wrbpod
-    
-    // publish the app's wrb rec to the name
-
+    while !terminate {
+        thread::sleep(Duration::from_secs(1));
+    }
 
     coord_channel
         .lock()
